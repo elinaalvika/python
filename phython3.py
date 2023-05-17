54c6add1f785893428ad6b52db20a235c0b8a1b8
elinaalvika <91666078+elinaalvika@users.noreply.github.com>
2023-05-17 22:01:42 +0300

    Update phython3.py

--- phython3.py
+++ phython3.py
@@ -34,5 +34,79 @@ def nonemtBurtus(teksts, cikBurti):  #4.uzd
 
 print(nonemtBurtus("mani sauc elīna",1))
 
-def
+def check_first_last(numbers):           #5.uzd
+    if numbers[0] == numbers[-1]:
+        return True
+    else:
+        return False
+
+number_x = [10, 20, 30, 40, 10]
+numbers_y = [75, 65, 35, 75, 30]
+
+result_x = check_first_last(number_x)
+result_y = check_first_last(numbers_y)
+
+print("Given list:", number_x)
+print("Result is", result_x)
+
+print("Given list:", numbers_y)
+print("Result is", result_y)
+
+def find_divisible_by_5(numbers):          #6.uzd
+    divisible_numbers = [num for num in numbers if num % 5 == 0]
+    return divisible_numbers
+
+given_list = [10, 20, 33, 46, 55]
+divisible_numbers = find_divisible_by_5(given_list)
+
+print("Given list is:", given_list)
+print("Divisible by 5:", ', '.join(map(str, divisible_numbers)))
+
+rows = 5          #8.uzd
+
+for i in range(1, rows + 1):
+    for j in range(i):
+        print(i, end=" ")
+    print()
+
+
+def check_palindrome(number):           #9.uzd
+    original_number = number
+    reverse_number = 0
+
+    while number > 0:
+        remainder = number % 10
+        reverse_number = reverse_number * 10 + remainder
+        number = number // 10
+
+    if original_number == reverse_number:
+        return True
+    else:
+        return False
+
+number1 = 121
+number2 = 125
+
+is_palindrome1 = check_palindrome(number1)
+is_palindrome2 = check_palindrome(number2)
+
+print("Original number:", number1)
+print("Yes. Given number is a palindrome number." if is_palindrome1 else "No. Given number is not a palindrome number.")
+
+print("Original number:", number2)
+print("Yes. Given number is a palindrome number." if is_palindrome2 else "No. Given number is not a palindrome number.")
+
+list1 = [10, 20, 25, 30, 35]     #10.uzd
+list2 = [40, 45, 60, 75, 90]
+
+result_list = [element for element in list1 if element in list2]
+
+print("Result list:", result_list)
+
+rows = 5    #14.uzd
+
+for i in range(rows, 0, -1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
 
